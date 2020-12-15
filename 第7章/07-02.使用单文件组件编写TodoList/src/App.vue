3578d44cfb3fc558188bd0f1cd8f9a3212f04c85
chenweiyan <chanweiyan007@gmail.com>
2020-12-15 23:47:24 +0800

    intro SCF: TodoList demo

--- 第7章/07-02.使用单文件组件编写TodoList/src/App.vue
+++ 第7章/07-02.使用单文件组件编写TodoList/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <!-- v-model 数据双向绑定 -->
     <input v-model="inputValue" />
+    <!-- click事件监听，处理函数 -->
     <button class="button" @click="handleAddItem">提交</button>
   </div>
   <ul>
@@ -14,25 +16,33 @@
 
 <script>
 import { reactive, ref } from 'vue';
+// 查分出来的子组件
 import ListItem from './components/ListItem';
 
 export default {
-  name: 'App',
-  components: { ListItem },
+  name: 'App', // 组件名称
+  components: { ListItem }, // 注册组件
   setup() {
+    // vue 面向数据编程
+    // ref，reactive 响应式数据
     const inputValue = ref('');
     const list = reactive([]);
 
+    // 向列表中添加数据
+    // 清空输入框
     const handleAddItem = () => {
       list.push(inputValue.value);
       inputValue.value = '';
     };
+    // TodoList
+    // 一个方法两个数据
+    //
     return { handleAddItem, inputValue, list }
   }
 }
 </script>
-
 <style>
+/* 没有 scoped，样式会作用在全局 */
   .button {
     margin-left: 20px;
     color: red;
--- 第7章/07-02.使用单文件组件编写TodoList/src/components/ListItem.vue
+++ 第7章/07-02.使用单文件组件编写TodoList/src/components/ListItem.vue
@@ -3,8 +3,11 @@
 </template>
 
 <script>
+// es6 module
 export default {
   name: 'ListItem',
+  // 接收的参数/属性
+  // 使用 props 实现组件的间数据传递
   props: {
     msg: String
   }
